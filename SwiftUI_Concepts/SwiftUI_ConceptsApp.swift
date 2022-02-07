ee6cec266004e7d468509606312d09cdcd459a10
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2022-02-07 19:49:06 +0530

    Initial Commit

---
+++ SwiftUI_Concepts/SwiftUI_ConceptsApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_ConceptsApp.swift
+//  SwiftUI_Concepts
+//
+//  Created by KANISHK VIJAYWARGIYA on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ConceptsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
